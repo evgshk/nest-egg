a434762b1a20da625881515ca11db915442380a0
Evgeny S <evgshk@gmail.com>
2019-07-21 00:27:03 +0300

    NE-9 Implement add saving method

--- src/app/shared/models/saving.model.ts
+++ src/app/shared/models/saving.model.ts
@@ -1,11 +1,12 @@
 import { Currency } from './currency.model';
+import * as firebase from 'firebase';
 
 export class Saving {
   id: string;
-  date: firebase.firestore.Timestamp;
-  currency: Currency;
-  type: string;
-  amount: number;
-  exchangeRate: number;
-  exchangeRateToday: number;
+  date: firebase.firestore.Timestamp = firebase.firestore.Timestamp.fromDate(new Date());
+  currency: Currency = new Currency();
+  type: string = '';
+  amount: number = 0;
+  exchangeRate: number = 0;
+  exchangeRateToday: number = 0;
 }
--- src/app/shared/services/firebase/saving.service.ts
+++ src/app/shared/services/firebase/saving.service.ts
@@ -20,4 +20,19 @@ export class SavingService {
         .orderBy('date', 'desc'))
       .valueChanges();
   }
+
+  add(item: Saving): string {
+    // TODO - refactor
+    const obj = Object.assign({}, item);
+    obj.currency = Object.assign({}, obj.currency);
+
+    const id = this.db.createId();
+    const documentRef = this.db
+      .collection<Saving>('savings')
+      .doc(id);
+      
+    documentRef.set(obj);
+
+    return id;
+  }
 }
